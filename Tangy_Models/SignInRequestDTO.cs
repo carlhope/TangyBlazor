1f908496b7ab297e22cdb79061d6dfc810d47a46
Carl hope <84421131+carlhope@users.noreply.github.com>
2024-06-14 15:52:49 +0100

    update

---
+++ Tangy_Models/SignInRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class SignInRequestDTO
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid Email Format")]
+        public string UserName { get; set; }
+        
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
