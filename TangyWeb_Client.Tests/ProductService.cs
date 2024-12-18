391d297f5ac3d71c4d26cb6f1fd2d8e556f4b999
Carl hope <84421131+carlhope@users.noreply.github.com>
2024-12-18 14:01:17 +0000

    unit test for products controller get all

---
+++ TangyWeb_Client.Tests/ProductService.cs
@@ -0,0 +1,16 @@
+namespace TangyWeb_Client.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetById()
+        {
+            Assert.Pass();
+        }
+    }
+}
